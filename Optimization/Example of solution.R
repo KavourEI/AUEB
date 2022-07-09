b350c25def51df40b00f720e1eeb0999a735367d
KavourEI <themis.kavour@icloud.com>
2022-07-09 14:10:19 +0300

    Question 6 almost completed

--- Optimization/Example of solution.R
+++ Optimization/Example of solution.R
@@ -1,7 +1,7 @@
 library(lpSolve)
 library(lpSolveAPI)
 
-f.obj <- c(40,30,-1)
+f.obj <- c(40,30,-105/100)
 f.con <- matrix(c(0.5,0.3,0,0.3,0.4,0,0.3,0.1,0,100,80,-1,160,130,-1,0,0,1,1,0,0,0,1,0,0,0,1), nrow = 9, byrow = TRUE)
 f.dir <- c("<=","<=","<=","<=","<=","<=",">=",">=",">=")
 f.rhs <- c(1800,1000,700,300000,300000,200000,0,0,0)
@@ -9,4 +9,16 @@ f.rhs <- c(1800,1000,700,300000,300000,200000,0,0,0)
 result <- lp("max",f.obj,f.con,f.dir,f.rhs, all.int =TRUE)
 
 result$solution
-result$objval - result$solution[3]
+result$objval
+
+
+
+f.obj <- c(40,30)
+f.con <- matrix(c(0.5,0.3,0.3,0.4,0.3,0.1,100,80,160,130,1,0,0,1), nrow = 7, byrow = TRUE)
+f.dir <- c("<=","<=","<=","<=","<=",">=",">=")
+f.rhs <- c(1800,1000,700,300000,300000,0,0)
+
+result <- lp("max",f.obj,f.con,f.dir,f.rhs, all.int =TRUE)
+
+result$solution
+result$objval
